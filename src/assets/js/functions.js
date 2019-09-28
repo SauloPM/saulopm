c8249532311231118ef4b5acb27ef0ac7f173dde
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-09-28 19:03:11 +0100

    [ General ] » Migración a Angular completada

---
+++ src/assets/js/functions.js
@@ -0,0 +1,20 @@
+$(window).on("load", function(){
+    
+  // WOW
+  new WOW({
+      mobile: false
+  }).init();  
+
+  // ––––––––––––––– //
+  //     SPINNER     //
+  // ––––––––––––––– //
+
+  hideSpinner();
+
+  function hideSpinner () {
+    $("#preloader .spinner").fadeOut(500, function () {
+        $("#preloader").delay(500).fadeOut(500);
+        setTimeout(function () { $("body").css("overflow", "visible"); }, 500);
+    });
+  }
+});
