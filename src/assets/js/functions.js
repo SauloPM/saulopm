7acae903359b4349a0116edad7bee44d561a7b2f
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-09-29 15:17:30 +0100

    [ Componente 'proyectos' ] » Estilos actualizados (animación en caption)

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -17,4 +17,37 @@ $(window).on("load", function(){
         setTimeout(function () { $("body").css("overflow", "visible"); }, 500);
     });
   }
+
+  // ––––––––––––––––– //
+  //     PROYECTOS     //
+  // ––––––––––––––––– //
+
+  $(document).on("mouseover", "#proyectos .proyecto", function() {
+    
+    $(this).find(".caption").animate({ opacity: 1 }, 250);
+
+    setTimeout(() => {
+      $(this).find(".caption .titulo").animate({ opacity: 1 }, 250);
+    }, 350);
+
+    setTimeout(() => {
+      $(this).find(".caption .herramientas").animate({ opacity: 1 }, 250);
+    }, 650);
+
+  });
+
+  $(document).on("mouseleave", "#proyectos .proyecto", function() {
+    
+    $(this).find(".caption .herramientas").animate({ opacity: 0 }, 250);
+
+    setTimeout(() => {
+      $(this).find(".caption .titulo").animate({ opacity: 0 }, 250);
+    }, 350);
+
+    setTimeout(() => {
+      $(this).find(".caption").animate({ opacity: 0 }, 250);
+    }, 650);
+
+  });
+
 });
