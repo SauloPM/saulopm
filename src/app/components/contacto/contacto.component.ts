b6ed4f8c5aa06808f3eede11df8b77b121d928e0
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2020-01-14 19:13:29 +0000

    [ Componente 'contacto' ] » Navegación suavizada

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,20 +1,59 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+// jQuery
+declare var $: any; 
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.scss']
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit {
 
-  @Output() dispararAperturaContacto: EventEmitter<boolean>;
+  constructor( private router: Router ) { }
 
-  constructor() {
-    this.dispararAperturaContacto = new EventEmitter();
+  ngOnInit() {
+
+    setTimeout(() => {
+      $('.cabecera').css( 'opacity', '1' );
+      $('.cierre').css( 'transform', 'scaleX(1)' );
+    }, 500);
+
+    setTimeout(() => {
+      $('.cierre .barra:first-child').css( 'transform', 'rotate( 45deg)' );
+      $('.cierre .barra:last-child' ).css( 'transform', 'rotate(-45deg)' );
+    }, 750);
+
+    setTimeout(() => {
+      $('.datos-contacto').css( 'opacity', '.5' );
+      $('.formulario').css( { 'opacity': '1', 'margin-top': '-135px' } );
+    }, 1000);
+
   }
 
-  cerrarContacto() {
-    this.dispararAperturaContacto.emit( false );
-  }
+  volverPaginaInicio() {
 
+    // Cierre
+    $('.cierre .barra:first-child').css( 'transform', 'rotate(0)' );
+    $('.cierre .barra:last-child' ).css( 'transform', 'rotate(0)' );
+
+    // Formulario
+    $('.formulario').css( { 'opacity': '0', 'margin-top': '-150px' } );
+
+    // Datos de contacto
+    $('.datos-contacto').css( 'opacity', '0' );
+    
+    setTimeout(() => {
+      $('.cierre').css( 'transform', 'scaleX(0)' );
+    }, 250);
+
+    setTimeout(() => {
+      $('.cabecera').css( 'opacity', '0' );
+    }, 500);
+
+    setTimeout(() => {
+      this.router.navigate( ['inicio'] );
+    }, 1500);
+  }
 }
