f33f269db876e30a3fa4b2b548a16578ca48b8bd
saulopm.1989@gmail.com <saulopm.1989@gmail.com>
2020-01-15 09:54:22 +0000

    [ Componente 'contacto' ] » Error de maquetación solucionado

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -24,44 +24,48 @@ export class ContactoComponent implements AfterViewInit, OnInit {
   ngOnInit() {
 
     // This data could really come from some inputs on the interface - but let's keep it simple
-    this.name    = "Saulo Poveda Montesdeoca";
-    this.email   = "info@saulopm.com";
-    this.message = "Hello, this is Hayden.";
+    this.name    = 'Saulo Poveda Montesdeoca';
+    this.email   = 'info@saulopm.com';
+    this.message = 'Hello, this is Hayden.';
 
     // Start PHP via the built in server: $ php -S localhost:8000
-    this.endpoint = "https://saulopm.com/proyectos/email/sendemail.php";
+    this.endpoint = 'https://saulopm.com/proyectos/email/sendemail.php';
   }
 
   ngAfterViewInit() {
 
-      setTimeout(() => {
-        $('.cabecera').css( 'opacity', '1' );
-        $('.cierre').css( 'transform', 'scaleX(1)' );
-      }, 500);
-  
-      setTimeout(() => {
-        $('.cierre .barra:first-child').css( 'transform', 'rotate( 45deg)' );
-        $('.cierre .barra:last-child' ).css( 'transform', 'rotate(-45deg)' );
-      }, 750);
-  
-      setTimeout(() => {
-        $('.datos-contacto').css( 'opacity', '.5' );
-        $('.formulario').css( { 'opacity': '1', 'margin-top': '-135px' } );
-      }, 1000);
+    const marginTop = parseInt( $( '.formulario' ).css( 'marginTop' ));
+
+    setTimeout(() => {
+      $('.cabecera').css( 'opacity', '1' );
+      $('.cierre').css( 'transform', 'scaleX(1)' );
+    }, 500);
+
+    setTimeout(() => {
+      $('.cierre .barra:first-child').css( 'transform', 'rotate( 45deg)' );
+      $('.cierre .barra:last-child' ).css( 'transform', 'rotate(-45deg)' );
+    }, 750);
+
+    setTimeout(() => {
+      $('.datos-contacto').css( 'opacity', '.5' );
+      $('.formulario').css( { 'opacity': '1', 'margin-top': marginTop - 15 } );
+    }, 1000);
   }
 
   volverPaginaInicio() {
+
+    const marginTop = parseInt( $( '.formulario' ).css( 'marginTop' ));
 
     // Cierre
     $('.cierre .barra:first-child').css( 'transform', 'rotate(0)' );
     $('.cierre .barra:last-child' ).css( 'transform', 'rotate(0)' );
 
     // Formulario
-    $('.formulario').css( { 'opacity': '0', 'margin-top': '-150px' } );
+    $('.formulario').css( { 'opacity': '0', 'margin-top': marginTop + 15 } );
 
     // Datos de contacto
     $('.datos-contacto').css( 'opacity', '0' );
-    
+
     setTimeout(() => {
       $('.cierre').css( 'transform', 'scaleX(0)' );
     }, 250);
@@ -77,16 +81,14 @@ export class ContactoComponent implements AfterViewInit, OnInit {
 
   enviarEmail() {
 
-    alert('Hola')
-
-    let postVars = {
+    const parametros = {
       name   : this.name,
       email  : this.email,
       message: this.message
     };
 
     // You may also want to check the response. But again, let's keep it simple
-    this.http.post( this.endpoint, postVars ).subscribe(
+    this.http.post( this.endpoint, parametros ).subscribe(
       response => console.log( response ),
       response => console.log( response )
     );
