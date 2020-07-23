6dbde6ca886adf58bd5d173909e930847e6fb0ef
Saulo Poveda Montesdeoca <saulo.poveda@dxc.com>
2020-07-23 23:00:37 +0100

    [ Proyectos ] » Distribución actualizada. Efecto 3D al hacer hover en la tarjeta de un proyecto incluido

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,16 +1,24 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+
+declare var VanillaTilt: any;
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.scss']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit {
 
   @Output() onAbrirModal: EventEmitter<string>
 
   constructor() {
     this.onAbrirModal = new EventEmitter();
+  }
+
+  ngOnInit() {
+
+    VanillaTilt.init( document.querySelectorAll( '.proyecto'), { max: 8, speed: 1000 });
+    
   }
 
   abrirModal( id: string ) {
